6a7998f01e0ddb64a7e0535a190cedaba5856ea0
Dan Peach <daniel.peach@armory.io>
2020-08-06 17:04:45 -0400

    add integration test

--- notification-agent-echo/src/test/kotlin/io/armory/plugin/example/echo/notificationagent/EchoPluginsFixture.kt
+++ notification-agent-echo/src/test/kotlin/io/armory/plugin/example/echo/notificationagent/EchoPluginsFixture.kt
@@ -3,16 +3,20 @@ package io.armory.plugin.example.echo.notificationagent
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.netflix.spinnaker.echo.Application
+import com.netflix.spinnaker.echo.services.Front50Service
 import com.netflix.spinnaker.kork.plugins.SpinnakerPluginManager
 import com.netflix.spinnaker.kork.plugins.internal.PluginJar
 import com.netflix.spinnaker.kork.plugins.tck.PluginsTckFixture
-//import com.squareup.okhttp.mockwebserver.MockWebServer
+import com.squareup.okhttp.mockwebserver.MockWebServer
+import io.mockk.every
+import io.mockk.mockk
 import java.io.File
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
@@ -47,9 +51,14 @@ class EchoPluginsFixture : PluginsTckFixture, EchoTestService() {
   @Autowired
   lateinit var mockMvc: MockMvc
 
-  val mapper = jacksonObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+  final val mapper = jacksonObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
-//  final val handler = MockWebServer()
+  // Usually you can define this value dynamically using the DynamicPropertyRegistry.
+  // This didn't seem to work with the plugin framework (the framework is responsible for
+  // mapping config between Spring and @PluginConfiguration annotations). Something to investigate.
+  // This value maps to the configured URL in echo-plugins-test.yml.
+  final val RECEIVER_PORT = 9999
+  final val receiver = MockWebServer()
 
   init {
     plugins.delete()
@@ -57,7 +66,6 @@ class EchoPluginsFixture : PluginsTckFixture, EchoTestService() {
     enabledPlugin = buildPlugin("io.armory.echo.enabled.plugin", ">=1.0.0")
     disabledPlugin = buildPlugin("io.armory.echo.disabled.plugin", ">=1.0.0")
     versionNotSupportedPlugin = buildPlugin("io.armory.echo.version.not.supported.plugin", ">=1000.0.0")
-//    handler.start(9000)
   }
 }
 
@@ -68,4 +76,9 @@ class EchoPluginsFixture : PluginsTckFixture, EchoTestService() {
 abstract class EchoTestService
 
 @TestConfiguration
-internal class PluginTestConfiguration
+internal class PluginTestConfiguration {
+  @Bean
+  fun front50Service(): Front50Service = mockk {
+    every { pipelines }.returns(emptyList())
+  }
+}
--- notification-agent-echo/src/test/kotlin/io/armory/plugin/example/echo/notificationagent/HTTPNotificationAgentIntegrationTest.kt
+++ notification-agent-echo/src/test/kotlin/io/armory/plugin/example/echo/notificationagent/HTTPNotificationAgentIntegrationTest.kt
@@ -1,8 +1,16 @@
 package io.armory.plugin.example.echo.notificationagent
 
+import com.netflix.spinnaker.echo.api.events.Event
+import com.netflix.spinnaker.echo.api.events.Metadata
 import com.netflix.spinnaker.kork.plugins.tck.PluginsTck
 import com.netflix.spinnaker.kork.plugins.tck.serviceFixture
+import com.squareup.okhttp.mockwebserver.MockResponse
 import dev.minutest.rootContext
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.post
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotNull
 
 class HTTPNotificationAgentIntegrationTest : PluginsTck<EchoPluginsFixture>() {
   fun tests() = rootContext<EchoPluginsFixture> {
@@ -13,44 +21,52 @@ class HTTPNotificationAgentIntegrationTest : PluginsTck<EchoPluginsFixture>() {
 
       defaultPluginTests()
 
-//      test("HTTPNotificationAgent handles an http notification event") {
-//        handler.enqueue(MockResponse().setResponseCode(200))
-//
-//        val message = mapper.writeValueAsString(Event().apply {
-//          details = Metadata().apply {
-//            source = "orca"
-//            type = "orca:stage:complete"
-//            application = "my-test-app"
-//          }
-//          content = mapOf(
-//            "execution" to mapOf(
-//              "type" to "PIPELINE",
-//              "application" to "my-test-app",
-//              "notifications" to listOf(
-//                mapOf(
-//                  "level" to "stage",
-//                  "type" to "http",
-//                  "when" to listOf("stage.complete")
-//                )
-//              )
-//            )
-//          )
-//        })
-//
-//        val response = mockMvc.post("/") {
-//          contentType = MediaType.APPLICATION_JSON
-//          content = message
-//        }.andReturn().response
-//
-//        expect {
-//          that(response.status).isEqualTo(200)
-//
-//          that(handler.requestCount).isEqualTo(1)
-//          that(handler.takeRequest()) {
-//            get { body.readUtf8() }.isEqualTo(message)
-//          }
-//        }
-//      }
+      test("agent handles an notification event, passes event to statically configured endpoint and dynamically configured path") {
+        receiver.start(RECEIVER_PORT)
+
+        receiver.enqueue(MockResponse().setResponseCode(200))
+
+        val response = mockMvc.post("/") {
+          contentType = MediaType.APPLICATION_JSON
+          content = mapper.writeValueAsString(Event().apply {
+            details = Metadata().apply {
+              source = "orca"
+              type = "orca:pipeline:complete"
+              application = "my-test-app"
+            }
+            content = mapOf(
+              "execution" to mapOf(
+                "type" to "PIPELINE",
+                "application" to "my-test-app",
+                "notifications" to listOf(
+                  mapOf(
+                    "level" to "pipeline",
+                    "type" to "http",
+                    "when" to listOf("pipeline.complete"),
+                    // The notification agent defines the key (see HTTPNotificationAgent#getParameters()).
+                    // The value (in a non-test context!) is user-defined within Spinnaker's UI.
+                    "path" to "/spinnaker-notifications"
+                  )
+                )
+              )
+            )
+          })
+        }.andReturn().response
+
+        expect {
+          that(response.status).isEqualTo(200)
+
+          that(receiver.takeRequest()) {
+            get { path }.isEqualTo("/spinnaker-notifications")
+            get { body.readUtf8() }.isNotNull()
+          }
+          that(receiver.requestCount).isEqualTo(1)
+        }
+
+        after {
+          receiver.shutdown()
+        }
+      }
     }
   }
 }
